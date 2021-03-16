14dd461d4d82d6c1ba5cb4909697ae3553fa5650
jaybamania <jaykumardiu@gmail.com>
2021-03-16 18:37:30 +0530

    Arrays

--- src/Components/YoutubeForm.js
+++ src/Components/YoutubeForm.js
@@ -13,7 +13,8 @@ const initialValues = {
     social:{
         facebook : '',
         twitter: ''
-    }
+    },
+    phoneNumbers : ['','']
 }
 
 const onSubmit = values =>{
@@ -92,6 +93,14 @@ function YoutubeForm() {
                 <div className='form-control'>
                     <label htmlFor='twitter'>Twitter Profile</label>
                     <Field type='text' id='twitter' name='social.twitter' />
+                </div>
+                <div className='form-control'>
+                    <label htmlFor='primaryPh'>Primary phone Number</label>
+                    <Field type='text' id='primaryPh' name='phoneNumbers[0]' />
+                </div>
+                <div className='form-control'>
+                    <label htmlFor='secondaryPh'>Secondary phone Number</label>
+                    <Field type='text' id='secondaryPh' name='phoneNumbers[1]' />
                 </div>
                 <button type='submit'>Submit</button>
             </Form>
